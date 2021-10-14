c54f91a0e762e986b2496707449cbf1ee91c4bc7
Evgen-Mutagen <jeka190@mail.ru>
2021-10-14 15:49:55 +0300

    Upload HW8

--- src/com/urise/webapp/model/Link.java
+++ src/com/urise/webapp/model/Link.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 
 public class Link {
     private final String name;
-    private final String urlыв;
+    private final String url;
 
     public Link(String name, String url) {
         Objects.requireNonNull(name, "name must not be null");
