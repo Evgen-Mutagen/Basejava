28c2f5d09e762b13ca5ea4cb2e0d74e1e5227e71
Evgen-Mutagen <jeka190@mail.ru>
2021-11-07 14:36:10 +0300

    Upload HW11

--- src/com/urise/webapp/MainConcurrency.java
+++ src/com/urise/webapp/MainConcurrency.java
@@ -64,14 +64,14 @@ public class MainConcurrency {
         Thread thread1 = new Thread() {
             public void run() {
                 synchronized (LOCK1) {
-                    System.out.println("Thread 1: locked resource 1");
+                    System.out.println("Thread 1: locked thread 1");
                     try {
                         Thread.sleep(100);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                     synchronized (LOCK2) {
-                        System.out.println("Thread 1: locked resource 2");
+                        System.out.println("Thread 1: locked thread 2");
                     }
                 }
             }
@@ -80,14 +80,14 @@ public class MainConcurrency {
         Thread thread2 = new Thread() {
             public void run() {
                 synchronized (LOCK2) {
-                    System.out.println("Thread 2: locked resource 2");
+                    System.out.println("Thread 2: locked thread 2");
                     try {
                         Thread.sleep(100);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                     synchronized (LOCK1) {
-                        System.out.println("Thread 2: locked resource 1");
+                        System.out.println("Thread 2: locked thread 1");
                     }
                 }
             }
