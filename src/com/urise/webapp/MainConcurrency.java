2e0b75a0a962ecf689b4024ae558e13f98763d08
Evgen-Mutagen <jeka190@mail.ru>
2021-11-07 14:32:35 +0300

    Upload HW11

--- src/com/urise/webapp/MainConcurrency.java
+++ src/com/urise/webapp/MainConcurrency.java
@@ -7,6 +7,8 @@ public class MainConcurrency {
     public static final int THREADS_NUMBER = 10000;
     private int counter;
     private static final Object LOCK = new Object();
+    private static final Object LOCK1 = new Object();
+    private static final Object LOCK2 = new Object();
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println(Thread.currentThread().getName());
@@ -58,6 +60,41 @@ public class MainConcurrency {
             }
         });
         System.out.println(mainConcurrency.counter);
+
+        Thread thread1 = new Thread() {
+            public void run() {
+                synchronized (LOCK1) {
+                    System.out.println("Thread 1: locked resource 1");
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized (LOCK2) {
+                        System.out.println("Thread 1: locked resource 2");
+                    }
+                }
+            }
+        };
+
+        Thread thread2 = new Thread() {
+            public void run() {
+                synchronized (LOCK2) {
+                    System.out.println("Thread 2: locked resource 2");
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized (LOCK1) {
+                        System.out.println("Thread 2: locked resource 1");
+                    }
+                }
+            }
+        };
+
+        thread1.start();
+        thread2.start();
     }
 
     private synchronized void inc() {
