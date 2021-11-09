ea3043f036fb4c5cad14ed2923a047a4e0d4aeec
Evgen-Mutagen <jeka190@mail.ru>
2021-11-09 10:27:33 +0300

    Upload HW11

--- src/com/urise/webapp/MainDeadlock.java
+++ src/com/urise/webapp/MainDeadlock.java
@@ -2,20 +2,18 @@ package com.urise.webapp;
 
 public class MainDeadlock {
         public void deadlock (Object lock1, Object lock2) {
-           new Thread() {
-               public void run() {
-                   synchronized (lock1) {
-                       System.out.println("Thread : locked LOCK1");
-                       try {
-                           Thread.sleep(100);
-                       } catch (InterruptedException e) {
-                           e.printStackTrace();
-                       }
-                       synchronized (lock2) {
-                           System.out.println("Thread : locked LOCK2");
-                       }
+           new Thread(() -> {
+               synchronized (lock1) {
+                   System.out.println("Thread : locked LOCK1");
+                   try {
+                       Thread.sleep(100);
+                   } catch (InterruptedException e) {
+                       e.printStackTrace();
+                   }
+                   synchronized (lock2) {
+                       System.out.println("Thread : locked LOCK2");
                    }
                }
-        }.start();
+           }).start();
     }
 }
