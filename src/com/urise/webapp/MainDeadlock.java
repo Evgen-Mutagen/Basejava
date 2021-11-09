3d139461b68c11f3bcfba6d57a0e2e4a9aef825e
Evgen-Mutagen <jeka190@mail.ru>
2021-11-09 21:06:19 +0300

    Upload HW11

--- src/com/urise/webapp/MainDeadlock.java
+++ src/com/urise/webapp/MainDeadlock.java
@@ -10,7 +10,7 @@ public class MainDeadlock {
 
     public static void deadlock(String lock1, String lock2) {
         new Thread(() -> {
-            System.out.println(Thread.currentThread().getName() + " are waiting for " + lock1);
+            System.out.println(Thread.currentThread().getName() + " is waiting for " + lock1);
             synchronized (lock1) {
                 System.out.println(Thread.currentThread().getName() + ": " + lock1);
                 try {
@@ -18,7 +18,7 @@ public class MainDeadlock {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                System.out.println(Thread.currentThread().getName() + " are waiting for " + lock2);
+                System.out.println(Thread.currentThread().getName() + " is waiting for " + lock2);
                 synchronized (lock2) {
                     System.out.println(Thread.currentThread().getName() + ": " + lock2);
                 }
